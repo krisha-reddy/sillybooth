417471c32ac047c3a1775269fc35d082013c2bbc
krisha-reddy <krishareddystudy@gmail.com>
2026-08-13 15:21:33 -0500

    layout and structure of the first page with logo and webcam element to take pictures

---
+++ script.js
@@ -0,0 +1,16 @@
+//accessing webcam using getUserMedia
+
+let video = document.querySelector("#videoElement");
+
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({video: true})
+    .then(function (stream) {
+        video.srcObject = stream;
+    }) 
+    .catch (function(error) {
+        console.log("Something went wrong");
+    })
+
+} else {
+    console.log("getUserMedia not supported on this device");
+}
