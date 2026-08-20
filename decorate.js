b52923789f355c7fe4aca701ae4101a329a3d7b2
Krisha Reddy Suresh <krishareddystudy@gmail.com>
2026-08-20 21:27:11 +0000

    changed structure

--- decorate.js
+++ decorate.js
@@ -1,9 +1,9 @@
 //display the saved photo
 const takenPic = sessionStorage.getItem('boothPic');
-const finalPic = document.getElementById('finalPic');
+const finalPhoto = document.getElementById('finalPic');
 
 if (takenPic) {
-    finalPic.src = takenPic;
+    finalPhoto.src = takenPic;
 } else {
     console.log("Picture not found!");
 }
